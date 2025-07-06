a54260e25ddc061dc9fc21eefdc03ed359ad67cf
maigotaisakuservice-spport-of-pcking <miikun2024@outlook.jp>
2025-07-06 13:15:14 +0900

    Update and rename enterprise.js to sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("gomi-cache-v1").then(cache => {
+      return cache.addAll(["./", "index.html"]);
+    })
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
